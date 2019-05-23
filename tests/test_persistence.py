6fde648899ac3e9f378b7c409a39c257f4fad6ac
Matteo Vettosi <matteo.vettosi@gmail.com>
2019-05-24 00:50:09 +0100

    Completed is_username_used tests

--- tests/test_persistence.py
+++ tests/test_persistence.py
@@ -20,19 +20,47 @@ TEST_USER_NAME = "username"
 TEST_SERVER_ID = "qwer1234"
 TEST_TOURNAMENT_SIZE = 4
 
-def test_is_username_used():
+def test_is_username_used_yes():
+    assert test_is_username_used()
+
+def test_is_username_used_wrong_server_id():
+    assert not test_is_username_used(server_id = TEST_SERVER_ID + 'a')
+
+def test_is_username_used_wrong_size():
+    assert not test_is_username_used(size = TEST_TOURNAMENT_SIZE + 1)
+
+def test_is_username_used_running_status():
+    assert not test_is_username_used(status = persistence.AnytimeStatus.RUNNING)
+
+def test_is_username_used_completed_status():
+    assert not test_is_username_used(status = persistence.AnytimeStatus.COMPLETED)
+
+def test_is_username_used_wrong_name():
+    assert not test_is_username_used(user_name = TEST_USER_NAME + 'a')
+
+def test_is_username_used_wrong_user_id():
+    assert test_is_username_used(user_id = TEST_USER_NAME + 'a')
+
+
+# Helper functions
+def test_is_username_used(
+    server_id = TEST_SERVER_ID,
+    size = TEST_TOURNAMENT_SIZE,
+    status = persistence.AnytimeStatus.RECRUITING,
+    user_name = TEST_USER_NAME,
+    user_id = TEST_USER_NAME):
     persistence.anytimes.insert({
-        'server_id': TEST_SERVER_ID,
-        'size': TEST_TOURNAMENT_SIZE,
-        'status': persistence.AnytimeStatus.RECRUITING,
+        'server_id': server_id,
+        'size': size,
+        'status': status,
         'players': [{
-            'user_id': TEST_USER_NAME,
-            'user_name': TEST_USER_NAME,
+            'user_id': user_id,
+            'user_name': user_name,
             'decks': None
         }]
     })
 
-    assert persistence.is_username_used(
+    return persistence.is_username_used(
         TEST_USER_NAME,
         TEST_SERVER_ID,
         TEST_TOURNAMENT_SIZE
